90930e4a23d8b80ab4bc09ad69b8da39c1ca323a
Seenixa <nepatomi@gmail.com>
2023-04-13 14:38:25 +0200

    newEmail

--- FormHandlers/NewEmail.php
+++ FormHandlers/NewEmail.php
@@ -1,10 +1,39 @@
 <?php
 session_start();
 include "../Classes/User.php";
+include "../Functions/SaveLoad.php";
 if (isset($_POST["emailchange"])) {
+    $user = loadIn($_SESSION["user"]);
+    $message = "";
 
     //ellenőrzések
 
-    $_SESSION["user"]->setEmail($_POST["email"]);
+    if (isset($_POST["email"]) && isset($_POST["repeatemail"])) {
+
+        if ($_POST["email"] == $_POST["repeatemail"]) {
+
+            if ($_POST["email"] != $user->getEmail()) {
+
+                if ($user->setEmail($_POST["email"])) {
+                    $_SESSION["user"]["email"] = $user->getEmail();
+                    $message = "Az E-mail cím változtatása sikeres";
+                } else {
+                    $message = "A megadott E-mail cím formailag helytelen";
+                }
+
+            } else {
+                $message = "Az új E-mail cím nem egyezhet meg a régivel";
+            }
+
+        } else {
+            $message = "A megadott E-mail címek nem egyeznek";
+        }
+
+    } else {
+        $message = "Töltsön ki minden mezőt az E-mail cím megváltoztatásához.";
+    }
+
+    echo $message;
+
 }
 ?>
